2d4c28e863eba51ba1e7f0a71b22fa583a6613d6
mohit kumar <107939086+mohitkumar357@users.noreply.github.com>
2023-01-10 14:19:38 +0530

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,159 +1,198 @@
-//Write the code to access element having id as "text"
-const text=document.getElementById("text");
-text.innerText="Hello changed";
-
-
-
-//Write the code to access element having tag as "h1"
-const text1=document.getElementsByTagName("h3");
-text1[0].innerText="hello again children";
-
-//Write the code to access element which is having code as "box"
-const box=document.getElementsByClassName("box");
-box[0].innerText="box again";
-
-//change the heading from "hello" to "hello world" using DOM functions
-
-const heading=document.getElementById("hello");
-  hello.addEventListener('mouseenter',function(){
-  hello.innerText="Hello World";
-})
-
-//Create three cards on html page and arrange them using flex property in row or horizontal direction and also create button at the botton
-//named "change flex direction".when user clicks on this button ,the cards arrangement should be changed to vertical direction.
-
-const btn=document.getElementById('btn');
-const cards=document.getElementsByClassName('cards');
-const card=document.getElementsByClassName('card');
-btn.addEventListener('click',function(){
-  cards[0].style.flexDirection="column";
-  btn.style.border="2px solid red";
-  btn.style.backgroundColor="aqua";
-  btn.innerText="changed";
-  console.log(cards);
-})
-
-//What is the difference between window vs document?
-
-//1.Document represents any HTML document or web page that is loaded in the browser while window represents a frame that displays the contents of the web page.
-//2.Document is loaded inside the window while window is loaded in the browser.
-//3.document is object of window property while window is object if the browser.
-
-//Add one style attribute in h1 and change text color as red and id attribute and give the id value as "heading" in the fiven h1 tag using DOM functions.
-
-const head=document.getElementsByTagName('h1');
-head[0].style.color="red";
-head[0].setAttribute("id","heading");
-console.log(document.getElementById('heading').id);
-
-//create a html page having content as hello world and a button named replace text.when user clicks on button ,content should be changed to 
-//"Welcome to Elevation Academy"
-
-const elem=document.getElementById('elevation');
-const ele=document.getElementById('btn-1');
-
-ele.addEventListener('click',function(){
-  elem.innerText="Welcome to Elevation Academy";
-  ele.innerText="content changed";
-})
-
-//Write a code to implement timer clock using JS--display HH:MM:SS--the time should keep updating every second
-
-function clock(){
-  let date=new Date();
-  let hrs=date.getHours();
-  let mins=date.getMinutes();
-  let secs=date.getSeconds();
-  let period="AM";
-
-  if(hrs==0){
-      hrs=12;
-  }
-  else if(hrs>12){
-      hrs=hrs-12;
-      period="PM";
-  }
-  hrs=hrs<10?`0${hrs}` : hrs;
-  mins=mins<10?`0${mins}`:mins;
-  secs=secs<10?`0${secs}`:secs;
-
-  let time=`${hrs}:${mins}:${secs}${period}`;
-  setInterval(clock,1000);
-  document.getElementById("clock").innerText=time;
-}
-
-clock();
-
-//create a dropdown for selecting time in morning like 2PM-3PM,3PM-4PM.print the item text selected.
-
-let morning=document.getElementById('morning');
-let morningText=morning.options[morning.selectedIndex].text;
-let output=document.getElementById('output');
-output.innerText=morningText;
-
-
-//Create a form having name,email,phone no,birth year.
-//add validations-phone no should start with 91,it should be 10 digits
-//email should be domain prepbytes.com
-//birth year should be>95
-
-function formDetails(){
-
-  var form=document.createElement("form");
-  var full_name=document.createElement("input");
-  document.write('<br>');
-  full_name.setAttribute("type","text");
-  full_name.setAttribute("name","full_name");
-  full_name.setAttribute("placeholder","fullname");
-  document.write('<br>');
-  document.write('<br>');
-
-
-  var email=document.createElement("input");
-  document.write('<br>');
-  email.setAttribute("type","email");
-  email.setAttribute("name","email");
-  email.setAttribute("placeholder","email@prepbytes.com");
-  document.write('<br>');
-  document.write('<br>');
-  
-   var phoneno=document.createElement("input");
-  document.write('<br>');
-  phoneno.setAttribute("type","text");
-  phoneno.setAttribute("name","phoneno");
-  phoneno.setAttribute("placeholder","+91 - ");
-  phoneno.setAttribute("maxlength","10");
-  document.write('<br>');
-  document.write('<br>');
-
-  var year=document.createElement("input");
-  document.write('<br>');
-  year.setAttribute("type","date");
-  year.setAttribute("name","year");
-  year.setAttribute("placeholder","DD/MM/YYYY");
-  year.setAttribute("min","1995-01-01");
-  document.write('<br>');
-  document.write('<br>');
-
-  var submit=document.createElement("input");
-  submit.setAttribute("type","submit");
+//Socks Draws
+const sockDraws = (n) => {
+    return n+1
+    
+ };
+ //Square Fit
+ const squareFit = (m,n) => {
+    if (m%2 ===0||n%2===0){
+      return (m*n)/2;
+    } 
+    else{
+      return(m*n-1)/2;
+    }
+ };
  
-  form.appendChild(full_name);
-  document.write('<br>');
+ 
+// Age calculation
+const ageCalculation = (f,s) => {
+    return Math.abs(f-2*s) ;
+ };
+ 
+// Atom Combine
+const atomCombine = (s) => {
+    return (2**s)-1;
+ };
+ //Discount Value
+ const discountValue = (p,d) => {
+    let discount =(p*d)/100;
+    let price = (p-discount);
+    return price;
+ };
+// Range Victory
+const LargestNumber = (l,r) => 
+{
+    if (l==r){
+      return l;
+    }
+    else{
+      return 1;
+    }
+};
 
-  form.appendChild(email);
-  document.write('<br>');
+//Divisibility Test
+const DivTest = (n) => 
+{
+   if(n%10 ===0){
+     return 0;
+   } 
+   else if ((n*2)%10===0)
+   {
+     return 1;
+   }
+   else{
+     return -1;
+   }
+};
+//Building Up Ten
+const BuildTen = (A,B) => {
+    if(A==10||B==10||A+B==10) {
+      return true;
+    }
+    else{
+      return false;
+    }
+ };
+// Play with Boolean
+const ChangeBool = (A) => 
+{
+   if(A=="true"||A==1){
+     return "0";
+   } 
+   else{
+     return "1";
+   }
+};
+//Checking Range
+const CheckRange = (A,B,C) => 
+{
+   if (Number.isInteger(A)&& (A>B && A<c)||(A>C && A<B) );
+   {
+     return true;
+   }
+   else{
+     return false;
+   }
+};
+//Pattern Puzzle
+const CompletePattern = (N) => 
+{
+    for(let r=0;r<=N;r++)
+    {
+      let r1="";
+      for(let c =0;c<r;c++)
+      {
+        r1+="x";
+      }
+      console.log(r1);
+    }
+};
 
-  form.appendChild(phoneno);
-  document.write('<br>');
+//Pattern Puzzle 1
 
-  form.appendChild(year);
-  document.write('<br>');
-  
-  form.appendChild(submit);
-  document.write('<br>');
+const CompletePattern = (N) => 
+{
+    
+    for(let r=0;r<=N;r++)
+    {
+      let r1="";
+      for(let c =0;c<r;c++)
+      {
+        r1+=r+" ";
+      }
+      console.log(r1);
+    }
+};
+//Pattern Puzzle 3
+const CompletePattern = (N) => 
+{
+    
+    for(let r=0;r<=N;r++)
+    {
+      let r1="";
+      for(let c =0;c<r;c++)
+      {
+        r1+=r+" ";
+      }
+      console.log(r1);
+    }
+//Pattern Puzzle 2
+const CompletePattern = (N) => 
+{
+    for(let i=1;i<=N;i++)
+    {
+      let space=" ";
+      for(let j=0;j<N-i;j++)
+      {
+        space+="?"
+      }
+      for(let j1=0;j1<2*i-1;j++)
+      {
+        space+="x"
+      }
+      for(let j2=0;j2<N-i;j2++)
+      {
+        space+="?"
+      }
+      console.log(space);
+    }
+};
+//Armstrong Number.
+const CheckArm = (N) => 
+{
+    let sum=0;
+    let t=N;
+    while(N!==0)
+    {
+      let num=N%10;
+      N=parseInt(N/10);
+      sum+=Math.pow(num,3)
+    }
+    if(sum ==t)
+    {
+      return 'Yes';
+    }
+    else{
+      return 'No';
+    }
+};
+//Unique Array
 
-  document.getElementsByClassName("form")[0].appendChild(form);
-  
-}
-formDetails();
+const UniqueElements = (N,arr) => 
+{
+    let res =[]
+    for(let i=0;i<N;i++)
+    {
+      for(var j=0;j<N;j++)
+      if(arr[i]==arr[j])
+      break;
+      if(i==j)
+      res.push(arr[i]);
+    }
+    console.log(res.join(' '));
+};
+
+//Find Duplicates
+const DuplicateElements = (N,arr) => 
+{
+    for(let i=0 ; i<N; i++)
+    {
+      for(let j=i+1;j<N;j++)
+      {
+        if(arr[i]==arr[j])
+        {
+          console.log(arr[i]);
+        }
+      }
+    }
+};
